5dd727317c207415784e44817fc50a5fd6f7fff3
suhyang-lee <suhyang.dev@gmail.com>
2021-04-18 17:19:24 +0900

    add header slide menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,7 @@
           ><img src="@/assets/images/m_logo.png" alt="VR미래기술박람회"
         /></a>
       </h1>
-      <nav class="lnb">
+      <nav class="lnb" @mouseover="openSlideMenu">
         <ul>
           <li>
             <a href="#">
@@ -41,6 +41,36 @@
         </ul>
       </nav>
     </header>
+
+    <div class="lnb_slide_menu" v-if="visible">
+      <div class="slide_menu" @mouseleave="closeSlideMenu">
+        <span class="empty"></span>
+        <ul>
+          <li>소개</li>
+          <li>찾아오시는 길</li>
+        </ul>
+        <ul>
+          <li>전시 안내</li>
+          <li>체험 프로그램</li>
+        </ul>
+        <ul>
+          <li>신청하기</li>
+          <li>확인하기</li>
+          <li>문의하기</li>
+        </ul>
+        <ul>
+          <li>사전등록안내</li>
+          <li>사전등록</li>
+          <li>사전등록확인</li>
+        </ul>
+        <ul>
+          <li>공지사항</li>
+          <li>보도자료</li>
+          <li>FAQ</li>
+          <li>Q&A</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -49,6 +79,21 @@ export default {
   name: 'Header',
   props: {
     msg: String,
+  },
+
+  data: function() {
+    return {
+      visible: false,
+    };
+  },
+
+  methods: {
+    openSlideMenu: function() {
+      this.visible = true;
+    },
+    closeSlideMenu: function() {
+      this.visible = false;
+    },
   },
 };
 </script>
