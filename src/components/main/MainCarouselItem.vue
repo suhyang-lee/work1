fa9ac460095e74c431879101919a60f742d4a0df
suhyang-lee <suhyang.dev@gmail.com>
2021-04-18 16:27:20 +0900

    update carousel contents, header hover effect

--- src/components/main/MainCarouselItem.vue
+++ src/components/main/MainCarouselItem.vue
@@ -1,5 +1,16 @@
 <template>
   <li :class="{ showing: idx === currentIdx }">
+    <div class="item_contents">
+      <div class="event">
+        <p>2017 VR/AR 국제 박람회</p>
+        <span
+          >미래 신기술 체험 프로그램 신청 안내 <br />
+          (초, 중,고 단체 및 개인)</span
+        >
+        <button class="event_btn">프로모션 바로가기</button>
+      </div>
+    </div>
+    <p>dfsdfsdfsfd</p>
     <img :src="require(`@/assets/images${url}`)" alt="#" />
   </li>
 </template>
